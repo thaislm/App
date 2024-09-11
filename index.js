070af40e20fb4877f21a60b0afbaafbff83d497f
Thaís Melo <thaismelo0608@gmail.com>
2024-09-11 13:26:09 -0400

    fundamentos 003

---
+++ index.js
@@ -0,0 +1,16 @@
+//arrays,objetos//
+
+let meta = {
+    value: 'Ler um livro todo mês',
+    checked: false,
+    isChecked: () => {}
+} 
+
+console.log(meta.value);
+
+
+//function // arrow function
+
+//const criarMeta =() => {}
+
+//function criarMeta (){}
